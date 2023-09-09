2282d818a314a826341493b29fb95a1e19c7104e
Nicholas Piedmonte <piedmonten@gmail.com>
2023-09-09 16:17:08 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,76 @@
+// Toggle mobile menu
+
+const toggle = document.querySelector('.toggle');
+const menu = document.querySelector('.menu');
+
+// Toggle mobile menu
+function toggleMenu() {
+    if (menu.classList.contains("active")) {
+        menu.classList.remove("active");
+
+        // adds the menu (hamburger) icon
+        toggle.querySelector("a").innerHTML = "<i class='fa fa-bars'></i>";
+    } else {
+        menu.classList.add("active");
+
+        // adds the close (x) icon
+        toggle.querySelector("a").innerHTML = "<i class='fa fa-close'></i>";
+    }
+}
+
+// Event listener for toggling nav menu with click
+toggle.addEventListener('click', toggleMenu, false);
+
+// Submenu
+const items = document.querySelectorAll(".item");
+
+// Activate submenu
+function toggleItem() {
+    if (this.classList.contains("submenu-active")) {
+        this.classList.remove("submenu-active");
+    } else if (menu.querySelector(".submenu-active")) {
+        menu.querySelector(".submenu-active").classList.remove("submenu-active");
+        this.classList.add("submenu-active");
+    } else {
+        this.classList.add("submenu-active");
+    }
+}
+
+// Add event listener to each item in items
+for (let item of items) {
+    if (item.querySelector('.submenu')) {
+        item.addEventListener('click', toggleItem, false);
+        item.addEventListener('keypress', toggleItem, false);
+    }
+}
+
+
+// Testimonial Slideshow
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("test-slides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
